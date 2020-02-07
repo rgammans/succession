5617e6cb9e27b18b37688a21c901a91f428d6397
Roger Gammans <rgammans@gammascience.co.uk>
2020-02-07 10:20:12 +0000

    Add tests and basic code for _run()

--- succession/job.py
+++ succession/job.py
@@ -7,11 +7,13 @@ async def nothing_to_do():
 class Job:
     def __init__(self,):
         self.dependencies = []
+        self.done = False
 
     def add_dependency(self, newdep):
         self.dependencies.append(newdep)
 
     def Ok(self):
+        """Return false if 'target' is not upto-date. Ie the Job needs to be run"""
         return ( self.check_self() and
                 all( d.Ok() for d in self.dependencies ))
 
@@ -23,8 +25,17 @@ class Job:
             return nothing_to_do()
 
     async def _run(self,):
-        pass
+        if self.done: return
+        dep_tasks = [
+             d.Run() for d in self.dependencies
+        ]
+        await asyncio.gather(*dep_tasks )
+        await self.do_run()
+        self.done = True
+        return None
 
     def check_self(self,):
         raise RuntimeError("You must override this with a function that retunrs false if you need to  run the task")
 
+    async def do_run(self,):
+        raise RuntimeError("You must override this to provide your task")
--- tests/test_job.py
+++ tests/test_job.py
@@ -1,27 +1,54 @@
 import unittest
 import unittest.mock
 import asyncio
+import aiounittest
 
 from succession.job import Job
 
-async def foo():pass
-foo = foo()
-coroutine = type(foo)
+async def empty_coroutine_gen():pass
+empty_coroutine = empty_coroutine_gen()
+coroutine = type(empty_coroutine)
 ## Silence Never awaited warnings
-try: foo.send(None)
+try: empty_coroutine.send(None)
 except: pass
 
 class MockJob(Job):
-    def __init__(self, ok):
+    def __init__(self, ok, task= None):
+        super().__init__()
+        self.has_run = False
         self._ok = ok
+        self.task = task
 
     def Ok(self):
         return self._ok
 
+    def Run(self,):
+        self.has_run = True
+        return empty_coroutine_gen()
+
+    async def do_run(self,):
+        if self.task:
+            await self.task()
+
+class AlwaysDoNothing(Job):
+    def check_self(self,):
+        return False
+
+    async def do_run(self,):
+        return None
 
 class BaseJobTests(unittest.TestCase):
     def setUp(self,):
-        self.j =  Job()
+        self.j =  AlwaysDoNothing()
+#
+#    def tearDown(self,):
+#        ## CLeanup event loop, which doesn't walasy happen
+#        loop = None
+#        try:
+#            loop = asyncio.get_event_loop()
+#        except RuntimeError:pass
+#        if loop:
+#            loop.close()
 
     def test_jobs_run_method_calls_Ok_first_and_doesnt_call_underrun_iftrue_return_a_completed_awaitbale(self,):
         with unittest.mock.patch.object(self.j,'Ok',return_value = True) as ok,\
@@ -90,3 +117,82 @@ class BaseJobTests(unittest.TestCase):
     def test_add_depency_adds_it_argument_to_depencies(self,):
         self.j.add_dependency(unittest.mock.sentinel.NEW_DEP)
         self.assertIn( unittest.mock.sentinel.NEW_DEP, self.j.dependencies,)
+
+
+    def test_dunder_run_call_Run_on_all_it_dependencies_when_they_need_to_run(self,):
+        deps = []
+        for i in range(10):
+            x = MockJob(False)
+            deps.append(x)
+            self.j.add_dependency(x)
+
+        self.j.do_run = empty_coroutine_gen
+        asyncio.run(self.j._run())
+        for x in deps:
+            self.assertTrue(x.has_run)
+
+    def test_dunder_run_call_Run_on_all_it_dependencies_when_they_dont_need_to_run(self,):
+        deps = []
+        for i in range(10):
+            x = MockJob(True)
+            deps.append(x)
+            self.j.add_dependency(x)
+
+        self.j.do_run = empty_coroutine_gen
+        asyncio.run(self.j._run())
+        for x in deps:
+            self.assertTrue(x.has_run)
+
+    def test_under_awaits_do_run_when_there_are_no_deps(self,):
+        with unittest.mock.patch.object(self.j,'do_run',return_value = empty_coroutine_gen()) as runner:
+            asyncio.run(self.j._run())
+
+        runner.assert_called_once_with()
+
+
+    @aiounittest.async_test
+    async def test_under_run_calls_do_run_only_after_deps_are_completed(self,):
+        ### Create some dependencies which we can use to pause the 
+        #   resolution.
+        deps = []
+
+        ## Create  task which can't complete .
+        block = asyncio.Lock()
+        def task():
+            with block:
+                return None
+
+        for i in range(10):
+            x = MockJob(False, task = task)
+            deps.append(x)
+            self.j.add_dependency(x)
+
+        await block.acquire()
+        with unittest.mock.patch.object(self.j,'do_run',return_value = empty_coroutine_gen()) as runner:
+            # Launch self.j._run()
+            completion = self.j._run()
+            # sleep.
+            await asyncio.sleep(1)
+#            await asyncio.sleep(1)
+            runner.assert_not_called()
+            # Release dependent blockers
+            block.release()
+            # Wait for _run() to complete
+            await completion
+            # Check j.do_run has_been_called
+            runner.assert_called_once_with()
+
+    @aiounittest.async_test
+    async def test_the_bottom_task_in_a_diamond_dependency_pattern_is_only_run_once(self,):
+        ## Build diamond
+        end_task = AlwaysDoNothing()
+        task_a = AlwaysDoNothing()
+        task_b = AlwaysDoNothing()
+        task_a.add_dependency(end_task)
+        task_b.add_dependency(end_task)
+        self.j.add_dependency(task_a)
+        self.j.add_dependency(task_b)
+        with unittest.mock.patch.object(end_task,'do_run',side_effect = empty_coroutine_gen) as runner:
+            await self.j._run()
+
+        runner.assert_called_once_with()
